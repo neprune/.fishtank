7cedfc18d2d14bec3ca3fdf874639cd333cd8560
Nik Gupta <vngupta77@gmail.com>
2026-02-19 12:14:53 +0000

    Capture swfind under mysoc.

---
+++ mysoc/functions/swfind.fish
@@ -0,0 +1,4 @@
+function swfind --wraps='gh search issues repo:mysociety/societyworks repo:mysociety/fixmystreet' --description 'alias swfind=gh search issues repo:mysociety/societyworks repo:mysociety/fixmystreet'
+  gh search issues repo:mysociety/societyworks repo:mysociety/fixmystreet $argv
+        
+end
